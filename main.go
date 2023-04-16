d92c7ef45c12a9a66f8a8e2a79ad25aae33c9f55
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-04-16 15:31:37 +0900

    On startup, print version, GOOS, GOARCH, and runtime-version

--- main.go
+++ main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime"
 )
 
+var version string
+
 func mains() error {
+	fmt.Printf("Sample %s-%s-%s by %s\n",
+		version, runtime.GOOS, runtime.GOARCH, runtime.Version())
 	return nil
 }
 
